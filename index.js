f1b40425dbb24c23f137114a1456d9de4309a8ab
Gautham-22 <gautham22poy@gmail.com>
2021-09-01 13:01:01 +0530

    added desktop layout for footer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let hamburger = document.getElementById("hamburger");
+let illustrationEditor = document.getElementById("illustration-editor");
 
 hamburger.addEventListener("click",() => {
     if(hamburger.classList.contains("active")) {
@@ -8,3 +9,25 @@ hamburger.addEventListener("click",() => {
     }
     hamburger.classList.toggle("active");
 })
+
+// for handling page layout when page loads
+if(window.innerWidth > 700) {
+    desktopLayout();
+}
+
+// for handling page layout when window resizes
+window.addEventListener("resize",() => {
+    if(window.innerWidth > 700) {
+        desktopLayout();
+    } else {
+        mobileLayout();
+    }
+})
+
+function mobileLayout() {
+    illustrationEditor.src = "./images/illustration-editor-mobile.svg";  
+} 
+
+function desktopLayout() {
+    illustrationEditor.src = "./images/illustration-editor-desktop.svg";  
+} 
